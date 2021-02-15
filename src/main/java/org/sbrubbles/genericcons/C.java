d4c73d87a56cd79fd94f00b3b62ac88ba93044de
Humberto Anjos <h.anjos@sbrubbles.org>
2021-02-15 20:49:50 -0300

    Doc updates.

--- src/main/java/org/sbrubbles/genericcons/C.java
+++ src/main/java/org/sbrubbles/genericcons/C.java
@@ -1,9 +1,8 @@
 package org.sbrubbles.genericcons;
 
 /**
- * Captures and represents an open-ended list of types. This class isn't 
- * supposed to be instantiated or subclassed; it's used to indicate a list in
- * the type declaration, like below:
+ * Captures and represents an open-ended list of types. This class isn't supposed to be instantiated or subclassed, 
+ * and has no fields or methods; it's a marker class, used to indicate a list in the type declaration, like below:
  * <p>
  * <pre>
  * Function&lt;?, ?&gt; f = new Function&lt;String, C&lt;Object, C&lt;Number, C&lt;String, Integer&gt;&gt;&gt;&gt;() {
@@ -13,8 +12,13 @@ package org.sbrubbles.genericcons;
  * };
  * </pre>
  * <p>
+ * In this example, {@code C&lt;Object, C&lt;Number, C&lt;String, Integer&gt;&gt;&gt;&gt;} encodes that {@code Function}'s
+ * second type argument is a list of types: {@code Object}, {@code Number}, {@code String} and {@code Integer}. The 
+ * {@link Types} class provides static methods to extract the types. 
+ * <p>
  * This list-like structure, similar to Lisp's {@code cons}, is named a <i>cons type</i> (hence the {@code C}), 
  * but using only one letter kept the whole structure more readable.
+ * <p>
  * 
  * @author Humberto Anjos
  * @param <First> The first type.
