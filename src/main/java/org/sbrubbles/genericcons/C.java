f3e37647078d94492d080c94d52f17db1e724de9
Humberto Anjos <h.anjos@sbrubbles.org>
2021-03-30 11:12:15 -0300

    Doc fixes.

--- src/main/java/org/sbrubbles/genericcons/C.java
+++ src/main/java/org/sbrubbles/genericcons/C.java
@@ -1,23 +1,27 @@
 package org.sbrubbles.genericcons;
 
+import java.lang.reflect.Type;
+
 /**
- * A marker class, used to build and represent an open-ended list of types in a generic declaration.
+ * A marker class, used to encode an open-ended list of types in a generic declaration.
  * Like below:
  * <p>
  * <pre>
- * Function&lt;?, ?&gt; f = new Function&lt;String, C&lt;Object, C&lt;Number, C&lt;String, Integer&gt;&gt;&gt;&gt;() {
+ * Fn&lt;?, ?&gt; f = new Fn&lt;String, C&lt;Object, C&lt;Number, C&lt;String, Integer&gt;&gt;&gt;&gt;() {
  *   // ...
  * };
  * </pre>
  * <p>
- * {@code C<Object, C<Number, C<String, Integer>>>>} encodes a list of types:
- * {@code Object}, {@code Number}, {@code String} and {@code Integer}. The {@link Types} class provides methods to
- * extract the types for later use.
+ * {@code C<Object, C<Number, C<String, Integer>>>>} represents a list of types:
+ * {@code Object}, {@code Number}, {@code String} and {@code Integer}.
  * <p>
- * This list-like structure is named a <i>cons</i> (hence the {@code C}), due to its similarities to Lisp's
- * {@code cons}. Using only one letter kept the whole structure more readable (hey, it could be worse).
+ * This list-like structure is named a <i>cons</i> (hence the {@code C}), due to its similarities to
+ * Lisp's {@code cons}. Using only one letter kept the whole thing more readable (hey, it could be worse).
  * <p>
- * This class isn't supposed to be instantiated or subclassed, and has no fields or methods.
+ * The {@link Types} class provides methods to {@linkplain Types#fromCons(Type) extract
+ * types from conses}.
+ * <p>
+ * This class isn't supposed to be instantiated or subclassed, and declares no fields or methods.
  * 
  * @author Humberto Anjos
  * @param <First> The first type.
