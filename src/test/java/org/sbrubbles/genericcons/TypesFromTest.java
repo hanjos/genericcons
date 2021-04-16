780aa5d09b11aa8b48d4af220d1e911768a0404f
Humberto Anjos <h.anjos@gmail.com>
2021-04-16 13:32:49 -0300

    Test refactoring.

--- src/test/java/org/sbrubbles/genericcons/TypesFromTest.java
+++ src/test/java/org/sbrubbles/genericcons/TypesFromTest.java
@@ -1,6 +1,5 @@
 package org.sbrubbles.genericcons;
 
-import com.coekie.gentyref.TypeFactory;
 import com.coekie.gentyref.TypeToken;
 import org.junit.Test;
 import org.sbrubbles.genericcons.fixtures.*;
@@ -8,237 +7,147 @@ import org.sbrubbles.genericcons.fixtures.*;
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import static org.junit.Assert.*;
+import static org.sbrubbles.genericcons.fixtures.Utils.listOf;
+import static org.sbrubbles.genericcons.fixtures.Utils.mapOf;
 
 public class TypesFromTest {
   @Test
   public void oneNonConsParameter() {
-    OneParameter<String> cons = new OneParameter<String>() { /**/ };
-    Object[] expected = new Object[] { String.class };
+    OneParameter<String> c = new OneParameter<String>() { /**/};
 
-    ParameterizedType actual = Types.genericSuperclassOf(cons.getClass()).get();
+    ParameterizedType supertype = Types.genericSuperclassOf(c.getClass()).get();
+
+    assertArrayEquals(
+      new Object[] {String.class},
+      Types.from(supertype, 0).toArray());
 
     assertEquals(
-      TypeFactory.parameterizedClass(OneParameter.class, String.class),
-      actual);
-
-    assertArrayEquals(
-      expected,
-      Types.from(actual, 0).toArray());
-
-    assertArrayEquals(
-      expected,
-      Types.fromSuperclass(cons.getClass(), 0).toArray());
+      Types.from(supertype, 0),
+      Types.fromSuperclass(c.getClass(), 0));
   }
 
   @Test
   public <T> void oneNonConsParameterWithTypeVariable() {
-    OneParameter<T> cons = new OneParameter<T>() { /**/ };
-    Type t = new TypeToken<T>() { /**/ }.getType();
-    Object[] expected = { t };
+    OneParameter<T> c = new OneParameter<T>() { /**/};
+    Type t = new TypeToken<T>() { /**/}.getType();
 
-    ParameterizedType actual = Types.genericSuperclassOf(cons.getClass()).get();
+    ParameterizedType supertype = Types.genericSuperclassOf(c.getClass()).get();
+
+    assertArrayEquals(
+      new Object[] {t},
+      Types.from(supertype, 0).toArray());
 
     assertEquals(
-      TypeFactory.parameterizedClass(OneParameter.class, t),
-      actual);
-
-    assertArrayEquals(
-      expected,
-      Types.from(actual, 0).toArray());
-
-    assertArrayEquals(
-      expected,
-      Types.fromSuperclass(cons.getClass(), 0).toArray());
+      Types.from(supertype, 0),
+      Types.fromSuperclass(c.getClass(), 0));
   }
 
   @Test
-  public void oneConsParameterWithTwoTypes() {
-    OneParameter<C<String, Number>> cons =
-      new OneParameter<C<String, Number>>() { /**/ };
-    Type typeList = encodeCons(String.class, Number.class);
-    Object[] expected = { String.class, Number.class };
+  public void oneConsParameter() {
+    OneParameter<C<String, C<Number, C<Object, C<Object[], Serializable>>>>> c =
+      new OneParameter<C<String, C<Number, C<Object, C<Object[], Serializable>>>>>() { /**/};
 
-    ParameterizedType actual = Types.genericSuperclassOf(cons.getClass()).get();
-
-    assertEquals(
-      TypeFactory.parameterizedClass(OneParameter.class, typeList),
-      actual);
+    ParameterizedType actual = Types.genericSuperclassOf(c.getClass()).get();
 
     assertArrayEquals(
-      expected,
-      Types.from(actual, 0).toArray());
-
-    assertArrayEquals(
-      expected,
-      Types.fromSuperclass(cons.getClass(), 0).toArray());
-  }
-
-  @Test
-  public void oneConsParameterWithFiveTypes() {
-    OneParameter<C<String, C<Number, C<Object, C<Object[], Serializable>>>>> cons =
-      new OneParameter<C<String, C<Number, C<Object, C<Object[], Serializable>>>>>() { /**/ };
-    Type typeList = encodeCons(String.class, Number.class, Object.class, Object[].class, Serializable.class);
-    Object[] expected = {
-      String.class,
-      Number.class,
-      Object.class,
-      new TypeToken<Object[]>() { /**/ }.getType(),
-      Serializable.class };
-
-    ParameterizedType actual = Types.genericSuperclassOf(cons.getClass()).get();
-
-    assertEquals(
-      TypeFactory.parameterizedClass(OneParameter.class, typeList),
-      actual);
-
-    assertArrayEquals(
-      expected,
-      Types.from(actual, 0).toArray());
-
-    assertArrayEquals(
-      expected,
-      Types.fromSuperclass(cons.getClass(), 0).toArray());
-  }
-
-  @Test
-  public void threeParameters() {
-    ThreeParameters<Number, String, C<Serializable, Serializable>> cons =
-      new ThreeParameters<Number, String, C<Serializable, Serializable>>() { /**/ };
-
-    ParameterizedType actual = Types.genericSuperclassOf(cons.getClass()).get();
-
-    assertEquals(
-      TypeFactory.parameterizedClass(ThreeParameters.class,
+      new Object[] {
+        String.class,
         Number.class,
-        String.class,
-        encodeCons(Serializable.class, Serializable.class)),
-      actual);
-
-    Object[] expected_1 = { String.class };
-
-    assertArrayEquals(
-      expected_1,
-      Types.from(actual, 1).toArray());
-
-    assertArrayEquals(
-      expected_1,
-      Types.fromSuperclass(cons.getClass(), 1).toArray());
-
-    Object[] expected_2 = { Serializable.class, Serializable.class };
-
-    assertArrayEquals(
-      expected_2,
-      Types.from(actual, 2).toArray());
-
-    assertArrayEquals(
-      expected_2,
-      Types.fromSuperclass(cons.getClass(), 2).toArray());
-  }
-
-  @Test
-  public void threeParametersSelectingTheConsWithFiveTypes() {
-    ThreeParameters<C<String, C<Number, C<Object, C<Object[], Serializable>>>>, String, String> cons =
-      new ThreeParameters<C<String, C<Number, C<Object, C<Object[], Serializable>>>>, String, String>() { /**/ };
-    Object[] expected = {
-      String.class,
-      Number.class,
-      Object.class,
-      TypeFactory.arrayOf(Object.class),
-      Serializable.class };
-
-    ParameterizedType actual = Types.genericSuperclassOf(cons.getClass()).get();
-
-    assertEquals(
-      TypeFactory.parameterizedClass(ThreeParameters.class,
-        encodeCons(String.class, Number.class, Object.class, Object[].class, Serializable.class),
-        String.class,
-        String.class),
-      actual);
-
-    assertArrayEquals(
-      expected,
+        Object.class,
+        new TypeToken<Object[]>() { /**/}.getType(),
+        Serializable.class},
       Types.from(actual, 0).toArray());
 
-    assertArrayEquals(
-      expected,
-      Types.fromSuperclass(cons.getClass(), 0).toArray());
+    assertEquals(
+      Types.from(actual, 0),
+      Types.fromSuperclass(c.getClass(), 0));
   }
 
   @Test
-  public void oneParameterWithConsWithCompositeTypes() {
+  public void threeParametersWithCons() {
+    ThreeParameters<Number, String, C<Serializable, Serializable>> cons =
+      new ThreeParameters<Number, String, C<Serializable, Serializable>>() { /**/};
+
+    ParameterizedType supertype = Types.genericSuperclassOf(cons.getClass()).get();
+
+    assertArrayEquals(
+      new Object[] {Number.class},
+      Types.from(supertype, 0).toArray());
+
+    assertEquals(
+      Types.from(supertype, 0),
+      Types.fromSuperclass(cons.getClass(), 0));
+
+    assertArrayEquals(
+      new Object[] {String.class},
+      Types.from(supertype, 1).toArray());
+
+    assertEquals(
+      Types.from(supertype, 1),
+      Types.fromSuperclass(cons.getClass(), 1));
+
+    assertArrayEquals(
+      new Object[] {Serializable.class, Serializable.class},
+      Types.from(supertype, 2).toArray());
+
+    assertEquals(
+      Types.from(supertype, 2),
+      Types.fromSuperclass(cons.getClass(), 2));
+  }
+
+  @Test
+  public void consWithCompositeTypes() {
     OneParameter<C<String, C<List<Number>, C<Object, Map<String, Integer>>>>> cons
-      = new OneParameter<C<String, C<List<Number>, C<Object, Map<String, Integer>>>>>() { /**/ };
-    Object[] expected = {
-      String.class,
-      listOf(Number.class),
-      Object.class,
-      mapOf(String.class, Integer.class) };
+      = new OneParameter<C<String, C<List<Number>, C<Object, Map<String, Integer>>>>>() { /**/};
 
     ParameterizedType actual = Types.genericSuperclassOf(cons.getClass()).get();
 
-    assertEquals(
-      TypeFactory.parameterizedClass(OneParameter.class,
-        encodeCons(String.class,
-          listOf(Number.class),
-          Object.class,
-          mapOf(String.class, Integer.class))),
-      actual);
-
     assertArrayEquals(
-      expected,
+      new Object[] {
+        String.class,
+        listOf(Number.class),
+        Object.class,
+        mapOf(String.class, Integer.class)},
       Types.from(actual, 0).toArray());
 
-    assertArrayEquals(
-      expected,
-      Types.fromSuperclass(cons.getClass(), 0).toArray());
+    assertEquals(
+      Types.from(actual, 0),
+      Types.fromSuperclass(cons.getClass(), 0));
   }
 
   @Test
   public void sonOfParameterizedType() {
     OneParameter<String> cons = new SonOfOneParameter();
-    Object[] expected = { String.class };
-
     ParameterizedType actual = Types.genericSuperclassOf(cons.getClass()).get();
 
-    assertEquals(
-      TypeFactory.parameterizedClass(OneParameter.class, String.class),
-      actual);
-
     assertArrayEquals(
-      expected,
+      new Object[] {String.class},
       Types.from(actual, 0).toArray());
 
-    assertArrayEquals(
-      expected,
-      Types.fromSuperclass(cons.getClass(), 0).toArray());
+    assertEquals(
+      Types.from(actual, 0),
+      Types.fromSuperclass(cons.getClass(), 0));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void grandsonOfParameterizedType() {
+    OneParameter<String> cons = new SonOfOneParameter() { /**/};
+
+    Types.fromSuperclass(cons.getClass(), 0);
   }
 
   @Test
-  public void grandsonOfParameterizedType() {
-    OneParameter<String> cons = new SonOfOneParameter() { /**/ };
-
-    Optional<ParameterizedType> actual = Types.genericSuperclassOf(cons.getClass());
-
-    assertFalse(actual.isPresent());
-
+  public void nullType() {
     try {
-      Types.fromSuperclass(cons.getClass(), 0);
-      fail("Types.fromSuperclass should've thrown IllegalArgumentException");
-    } catch(IllegalArgumentException e) {
+      Types.from(null, 0);
+      fail("Types.from should've thrown IllegalArgumentException");
+    } catch (IllegalArgumentException e) {
       /* if we're here, we're good */
     }
-  }
-
-  @Test
-  public void nullBaseClass() {
-    assertFalse(Types.genericSuperclassOf(null).isPresent());
-    assertFalse(Types.genericInterfaceOf(null, 0).isPresent());
 
     try {
       Types.fromSuperclass(null, 0);
@@ -257,143 +166,74 @@ public class TypesFromTest {
 
   @Test
   public void invalidIndex() {
-    OneParameter<String> cons = new OneParameter<String>() { /**/ };
-
-    ParameterizedType actual = Types.genericSuperclassOf(cons.getClass()).get();
-
-    assertEquals(
-      TypeFactory.parameterizedClass(OneParameter.class, String.class),
-      actual);
+    OneParameter<String> cons = new OneParameter<String>() { /**/};
 
     try {
-      Types.from(actual, -1);
+      Types.from(Types.genericSuperclassOf(cons.getClass()).get(), -1);
       fail("Types.from with genericSuperclassOf should've thrown IllegalArgumentException");
-    } catch(IllegalArgumentException e) {
+    } catch (IllegalArgumentException e) {
       /* if we're here, we're good */
     }
 
     try {
       Types.fromSuperclass(cons.getClass(), -1);
       fail("Types.fromSuperclass should've thrown IllegalArgumentException");
-    } catch(IllegalArgumentException e) {
+    } catch (IllegalArgumentException e) {
       /* if we're here, we're good */
     }
   }
 
   @Test(expected = IllegalArgumentException.class)
-  public void noTypeGiven() {
-    Types.from(null, 0);
+  public void fromSuperclassDoesntWorkWithInterfaces() {
+    IOneParameter<String> cons = new IOneParameter<String>() { /**/};
+    Types.fromSuperclass(cons.getClass(), 0);
   }
 
   @Test
-  public void nonParameterizedSuperclass() {
-    Optional<ParameterizedType> actual = Types.genericSuperclassOf(String.class);
-
-    assertFalse(actual.isPresent());
-
-    try {
-      Types.fromSuperclass(String.class, -1);
-      fail("Types.fromSuperclass should've thrown IllegalArgumentException");
-    } catch(IllegalArgumentException e) {
-      /* if we're here, we're good */
-    }
-  }
-
-  @Test
-  public void genericSuperclassOfDoesntWorkWithInterfaces() {
-    IOneParameter<String> cons = new IOneParameter<String>() { /**/ };
-    Optional<ParameterizedType> actual = Types.genericSuperclassOf(cons.getClass());
-
-    assertFalse(actual.isPresent());
-
-    try {
-      Types.fromSuperclass(cons.getClass(), 0);
-      fail("Types.fromSuperclass should've thrown IllegalArgumentException");
-    } catch(IllegalArgumentException e) {
-      /* if we're here, we're good */
-    }
-  }
-
-  @Test
-  public void genericInterfaceWorksWithInterfaces() {
-    IOneParameter<String> cons = new IOneParameter<String>() { /**/ };
-    Object[] expected = { String.class };
+  public void fromInterfaceWorksWithInterfaces() {
+    IOneParameter<String> cons = new IOneParameter<String>() { /**/};
 
     ParameterizedType actual = Types.genericInterfaceOf(cons.getClass(), 0).get();
 
-    assertEquals(
-      TypeFactory.parameterizedClass(IOneParameter.class, String.class),
-      actual);
-
     assertArrayEquals(
-      expected,
+      new Object[] {String.class},
       Types.from(actual, 0).toArray());
 
+    assertEquals(
+      Types.from(actual, 0),
+      Types.fromInterface(cons.getClass(), 0));
+  }
+
+  @Test
+  public void fromInterfaceGetsOnlyTheFirstInterface() {
+    ClassWithMultipleInterfaces c = new ClassWithMultipleInterfaces();
+    ParameterizedType actual = Types.genericInterfaceOf(c.getClass(), 0).get();
+
     assertArrayEquals(
-      expected,
-      Types.fromInterface(cons.getClass(),0).toArray());
+      new Object[] {String.class},
+      Types.from(actual, 0).toArray());
+
+    assertEquals(
+      Types.from(actual, 0),
+      Types.fromInterface(c.getClass(), 0));
   }
 
   @Test
-  public void genericInterfaceFailsIfNoInterfaceIsFoundAtIndex() {
-    IOneParameter<String> cons = new IOneParameter<String>() { /**/ };
-    Optional<ParameterizedType> actual = Types.genericInterfaceOf(cons.getClass(), 1);
-
-    assertFalse(actual.isPresent());
-  }
-
-  @Test
-  public void gettingTheRightGenericSuperinterface() {
-    assertEquals(
-      TypeFactory.parameterizedClass(Comparable.class, String.class),
-      Types.genericInterfaceOf(String.class, 1).get());
-
-    ClassWithMultipleInterfaces cons = new ClassWithMultipleInterfaces();
-
-    assertEquals(
-      TypeFactory.parameterizedClass(IOneParameter.class, String.class),
-      Types.genericInterfaceOf(cons.getClass(), 0).get());
-
-    assertEquals(
-      TypeFactory.parameterizedClass(ITwoParameters.class, Integer.class, listOf(Double.class)),
-      Types.genericInterfaceOf(cons.getClass(), 1).get());
-  }
-
-  @Test
-  public void nonParameterizedInterface() {
-    assertFalse(Types.genericInterfaceOf(String.class, 0).isPresent());
-
+  public void nonParameterizedSupertypes() {
     try {
-      Types.fromInterface(String.class, 0);
-      fail("Types.fromInterface should've thrown IllegalArgumentException");
-    } catch(IllegalArgumentException e) {
+      Types.fromSuperclass(String.class, 0);
+      fail("Types.fromSuperclass should've thrown IllegalArgumentException");
+    } catch (IllegalArgumentException e) {
       /* if we're here, we're good */
     }
-  }
 
-  @Test
-  public void invalidIndexGenericSuperinterface() {
-    ClassWithMultipleInterfaces cons = new ClassWithMultipleInterfaces();
-
-    assertFalse(Types.genericInterfaceOf(cons.getClass(), -1).isPresent());
-    assertFalse(Types.genericInterfaceOf(cons.getClass(), 3).isPresent());
-  }
-
-  // doesn't work for length == 0, but close enough
-  private Type encodeCons(Type... types) {
-    if (types.length == 1) {
-      return types[0];
+    try {
+      // XXX the first interface is Serializable, which is not generic, so this call should fail.
+      // Comparable<String> is the second interface
+      Types.fromInterface(String.class, 0);
+      fail("Types.fromInterface should've thrown IllegalArgumentException");
+    } catch (IllegalArgumentException e) {
+      /* if we're here, we're good */
     }
-
-    Type[] rest = Arrays.copyOfRange(types, 1, types.length);
-    return TypeFactory.parameterizedClass(C.class, types[0], encodeCons(rest));
-  }
-
-  private Type listOf(Type type) {
-    return TypeFactory.parameterizedClass(List.class, type);
-  }
-
-  private Type mapOf(Type key, Type value) {
-    return TypeFactory.parameterizedClass(Map.class, key, value);
   }
 }
---
+++ src/test/java/org/sbrubbles/genericcons/TypesSuppliersTest.java
@@ -0,0 +1,150 @@
+package org.sbrubbles.genericcons;
+
+import com.coekie.gentyref.TypeToken;
+import org.junit.Test;
+import org.sbrubbles.genericcons.fixtures.*;
+
+import java.io.Serializable;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.sbrubbles.genericcons.fixtures.Utils.*;
+
+
+public class TypesSuppliersTest {
+  @Test
+  public void genericSuperclassOfOneNonConsParameter() {
+    OneParameter<String> c = new OneParameter<String>() { /**/};
+
+    assertEquals(
+      type(OneParameter.class, String.class),
+      Types.genericSuperclassOf(c.getClass()).get());
+  }
+
+  @Test
+  public <T> void genericSuperclassOfOneParameterWithTypeVariable() {
+    OneParameter<T> c = new OneParameter<T>() { /**/};
+    Type t = new TypeToken<T>() { /**/}.getType();
+
+    assertEquals(
+      type(OneParameter.class, t),
+      Types.genericSuperclassOf(c.getClass()).get());
+  }
+
+  @Test
+  public void genericSuperclassOfOneParameterWithCons() {
+    OneParameter<C<String, C<Number, C<Object, C<Object[], Serializable>>>>> c =
+      new OneParameter<C<String, C<Number, C<Object, C<Object[], Serializable>>>>>() { /**/};
+
+    assertEquals(
+      type(OneParameter.class,
+           cons(String.class, Number.class, Object.class, Object[].class, Serializable.class)),
+      Types.genericSuperclassOf(c.getClass()).get());
+  }
+
+  @Test
+  public void genericSuperclassOfThreeParameters() {
+    ThreeParameters<Number, String, C<Serializable, Serializable>> c =
+      new ThreeParameters<Number, String, C<Serializable, Serializable>>() { /**/};
+
+    assertEquals(
+      type(ThreeParameters.class,
+           Number.class,
+           String.class,
+           cons(Serializable.class, Serializable.class)),
+      Types.genericSuperclassOf(c.getClass()).get());
+  }
+
+  @Test
+  public void genericSuperclassOfOneParameterWithConsAndCompositeTypes() {
+    OneParameter<C<String, C<List<Number>, C<Object, Map<String, Integer>>>>> c
+      = new OneParameter<C<String, C<List<Number>, C<Object, Map<String, Integer>>>>>() { /**/};
+
+    assertEquals(
+      type(OneParameter.class,
+           cons(String.class,
+                listOf(Number.class),
+                Object.class,
+                mapOf(String.class, Integer.class))),
+      Types.genericSuperclassOf(c.getClass()).get());
+  }
+
+  @Test
+  public void genericSuperclassOfSonOfParameterizedType() {
+    OneParameter<String> c = new SonOfOneParameter();
+
+    assertEquals(
+      type(OneParameter.class, String.class),
+      Types.genericSuperclassOf(c.getClass()).get());
+  }
+
+  @Test
+  public void genericSuperclassOfGrandsonOfParameterizedType() {
+    OneParameter<String> c = new SonOfOneParameter() { /**/};
+
+    assertFalse(Types.genericSuperclassOf(c.getClass()).isPresent());
+  }
+
+  @Test
+  public void genericSuperclassOfDoesntWorkWithInterfaces() {
+    IOneParameter<String> c = new IOneParameter<String>() { /**/};
+
+    assertFalse(Types.genericSuperclassOf(c.getClass()).isPresent());
+  }
+
+  @Test
+  public void genericInterfaceOfWorksWithInterfaces() {
+    IOneParameter<String> c = new IOneParameter<String>() { /**/};
+
+    assertEquals(
+      type(IOneParameter.class, String.class),
+      Types.genericInterfaceOf(c.getClass(), 0).get());
+  }
+
+  @Test
+  public void genericInterfaceFailsIfNoInterfaceIsFoundAtIndex() {
+    IOneParameter<String> c = new IOneParameter<String>() { /**/};
+
+    assertFalse(Types.genericInterfaceOf(c.getClass(), 1).isPresent());
+  }
+
+  @Test
+  public void gettingTheRightGenericSuperinterface() {
+    assertEquals(
+      type(Comparable.class, String.class),
+      Types.genericInterfaceOf(String.class, 1).get());
+
+    ClassWithMultipleInterfaces c = new ClassWithMultipleInterfaces();
+
+    assertEquals(
+      type(IOneParameter.class, String.class),
+      Types.genericInterfaceOf(c.getClass(), 0).get());
+
+    assertEquals(
+      type(ITwoParameters.class, Integer.class, listOf(Double.class)),
+      Types.genericInterfaceOf(c.getClass(), 1).get());
+  }
+
+  @Test
+  public void nonParameterizedSupertypes() {
+    assertFalse(Types.genericSuperclassOf(String.class).isPresent());
+    assertFalse(Types.genericInterfaceOf(String.class, 0).isPresent());
+  }
+
+  @Test
+  public void nullBaseClass() {
+    assertFalse(Types.genericSuperclassOf(null).isPresent());
+    assertFalse(Types.genericInterfaceOf(null, 0).isPresent());
+  }
+
+  @Test
+  public void invalidIndexGenericSuperinterface() {
+    ClassWithMultipleInterfaces c = new ClassWithMultipleInterfaces();
+
+    assertFalse(Types.genericInterfaceOf(c.getClass(), -1).isPresent());
+    assertFalse(Types.genericInterfaceOf(c.getClass(), 3).isPresent());
+  }
+}
